2372cf632f52c6cf6b5f60fee29c250d5c891aab
Frank Andrade <fr4nky.develop3r@gmail.com>
2020-03-28 18:41:11 -0400

    feature: create Styles SignIn and OrderConfirmation Components

--- src/Components/FonditaCustomer/OrderConfirmation.js
+++ src/Components/FonditaCustomer/OrderConfirmation.js
@@ -1,15 +1,58 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { isEmpty } from 'lodash';
 
 import { firestore } from '../../firebase';
 import { getDay } from '../../utilities';
+import { colors } from '../../colors';
+
+const OrderDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: vertical;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  h4 {
+    margin-top: 64px;
+    color: ${colors.red};
+  }
+  .Row {
+    margin-bottom: 8px;
+  }
+  .Customer {
+    margin-top: 24px;
+    color: ${colors.grayStrong};
+    width: 80%;
+  }
+  .OrderDetails {
+    margin-top: 16px;
+    width: 80%;
+    color: ${colors.grayStrong};
+    ul {
+      margin-top: 8px;
+      list-style: none;
+      margin-bottom: 8px;
+    }
+    span {
+      padding-left: 40px;
+    }
+    .OrderDetailsTitle {
+      padding-left: 0;
+    }
+  }
+  .OtherInfoOrder {
+    margin-top: 16px;
+    width: 80%;
+    color: ${colors.grayStrong};
+  }
+`;
 
 const OrderConfirmation = () => {
-  const order = useSelector((state) => state.orders);
-  const [orderDB, setOrder] = useState(order);
   const { id } = useParams();
+  const order = useSelector((state) => state.orders);
+  const [orderDB, setOrder] = useState(order[id]);
 
   useEffect(() => {
     const docRef = firestore
@@ -33,8 +76,6 @@ const OrderConfirmation = () => {
       });
   }, []);
 
-  console.log(orderDB);
-
   const getHourDelivery = (deliverPriority) => {
     switch (deliverPriority) {
       case 1:
@@ -56,33 +97,38 @@ const OrderConfirmation = () => {
     }
   };
 
+  const getTimeOrder = (time) => {
+    const re = /\d+:\d+/;
+    return re.exec(time);
+  };
+
   return (
     <div>
       {isEmpty(orderDB) ? (
         <div>What would you like to get =) </div>
       ) : (
-        <div>
+        <OrderDetailsContainer>
           <h4>Order Details</h4>
           <div className="Customer">
-            <div>
+            <div className="Row">
               <span>Customer: {orderDB.infoCustomer.customerName}</span>
             </div>
-            <div>
+            <div className="Row">
               <span>Address: {orderDB.infoCustomer.customerAddress}</span>
             </div>
-            <div>
+            <div className="Row">
               <span>
                 Office or Apt: {orderDB.infoCustomer.customerofficeOrApt}
               </span>
             </div>
             <div>
-              <span>
+              <span className="Row">
                 Phone Number: {orderDB.infoCustomer.customerPhoneNumber}
               </span>
             </div>
           </div>
           <div className="OrderDetails">
-            <span>Order details</span>
+            <span className="OrderDetailsTitle">Order details</span>
             <ul>
               {Object.values(orderDB.dishes).map((dish) => (
                 <li key={dish.dishID}>
@@ -90,15 +136,24 @@ const OrderConfirmation = () => {
                 </li>
               ))}
             </ul>
-            <span>Total Order: ${orderDB.totalOrder}</span>
+            <div className="Row">
+              <span>Total Order: ${orderDB.totalOrder}</span>
+            </div>
           </div>
           <div className="OtherInfoOrder">
-            <span>Time order: {orderDB.timeOrder}</span>
-            <span>
-              Hour delivery: {getHourDelivery(orderDB.deliverPriority)}
-            </span>
+            <div className="Row">
+              <span>
+                Time order: {getTimeOrder(orderDB.timeOrder)}
+                {''}
+              </span>
+            </div>
+            <div>
+              <span>
+                Hour delivery: {getHourDelivery(orderDB.deliverPriority)}
+              </span>
+            </div>
           </div>
-        </div>
+        </OrderDetailsContainer>
       )}
     </div>
   );
--- src/Components/FonditaOwner/SignIn.js
+++ src/Components/FonditaOwner/SignIn.js
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-console */
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import { keyBy, isEmpty } from 'lodash';
@@ -10,6 +12,44 @@ import setUser from '../../actions/authedUser';
 import { getDay } from '../../utilities';
 // eslint-disable-next-line import/no-cycle
 import Dashboard from './Dashboard';
+import { Button, InputContainer, ErrorInput } from '../../styled-components';
+import { colors } from '../../colors';
+
+const SignInContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const SignInForm = styled.div`
+  width: 350px;
+  color: ${colors.grayStrong};
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  .Form {
+    width: 80%;
+  }
+  .SignInTitle {
+    text-align: center;
+  }
+
+  @media (min-width: 768px) {
+    box-shadow: 0 0 0 1px #dc35351c, 0 1px 5px 0 rgba(163, 41, 41, 0.08);
+    border: 1px solid rgba(67, 41, 163, 0.2);
+    border-radius: 5px;
+    justify-content: center;
+  }
+`;
+
+const SubmitContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 32px;
+`;
 
 const SignIn = (props) => {
   const authedUser = useSelector((state) => state.authedUser);
@@ -68,6 +108,9 @@ const SignIn = (props) => {
         if (code === 'auth/user-not-found') {
           setInvalidEmail('There is no user corresponding to the given email');
         }
+        if (code === 'auth/too-many-requests') {
+          setInvalidPassword('Too many requests, try again later');
+        }
       });
   };
 
@@ -80,40 +123,64 @@ const SignIn = (props) => {
     }
   };
 
-  // console.log(authedUser);
+  console.log(invalidPassword);
   return (
-    <div className="SignIn">
+    <SignInContainer>
       {isEmpty(authedUser) ? (
-        <div className="SignIn2">
-          <div>Sign in</div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-              name="email"
-              placeholder="Your email"
-              ref={register({
-                required: true,
-                pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              })}
-              onChange={handleOnChange}
-            />
-            {errors.email && <span>Please put a valid email</span>}
-            <div>{invalidEmail}</div>
-            <input
-              name="password"
-              placeholder="Your password"
-              ref={register({ required: true })}
-              type="password"
-              onChange={handleOnChange}
-            />
-            {errors.password && <span>Please put your password</span>}
-            <div>{invalidPassword}</div>
-            <input type="submit" />
-          </form>
-        </div>
+        <SignInForm>
+          <h4 className="SignInTitle">Sign in</h4>
+
+          <div className="Form">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <InputContainer>
+                <label htmlFor="email">Email</label>
+                <input
+                  name="email"
+                  placeholder="Your email"
+                  ref={register({
+                    required: true,
+                    pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  })}
+                  onChange={handleOnChange}
+                />
+              </InputContainer>
+              <ErrorInput>
+                <span>{invalidEmail}</span>
+              </ErrorInput>
+              {errors.email && (
+                <ErrorInput>
+                  <span>Please put a valid email</span>
+                </ErrorInput>
+              )}
+
+              <InputContainer>
+                <label htmlFor="password">Password</label>
+                <input
+                  name="password"
+                  placeholder="Your password"
+                  ref={register({ required: true })}
+                  type="password"
+                  onChange={handleOnChange}
+                />
+              </InputContainer>
+              {errors.password && (
+                <ErrorInput>
+                  <span>Please put your password</span>
+                </ErrorInput>
+              )}
+              <ErrorInput>
+                <span>{invalidPassword}</span>
+              </ErrorInput>
+              <SubmitContainer>
+                <Button>Sign in</Button>
+              </SubmitContainer>
+            </form>
+          </div>
+        </SignInForm>
       ) : (
         <Dashboard />
       )}
-    </div>
+    </SignInContainer>
   );
 };
 
