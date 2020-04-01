6532b7e5eef86e4238fe4a2df2ad1ef664ab6a23
Frank Andrade <fr4nky.develop3r@gmail.com>
2020-03-31 20:02:14 -0400

    feature: create Styles tablet and desktop for PlacesAutocomplete component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../colors';
+
 const FooterContainer = styled.div`
   position: fixed;
   bottom: 0;
@@ -15,13 +16,37 @@ const FooterContainer = styled.div`
   justify-content: center;
   align-items: center;
 
+  @media (min-width: 768px) {
+    font-size: 28px;
+    margin-bottom: 8px;
+  }
+
+  @media (min-width: 1200px) {
+    font-size: 18px;
+  }
+
   .LinkFooter {
     color: ${colors.red};
     text-decoration: none;
     padding-left: 3px;
+    @media (min-width: 768px) {
+      padding-left: 6px;
+    }
+
+    @media (min-width: 1200px) {
+      padding-left: 4px;
+    }
   }
   .Heart {
     padding-left: 3px;
+    @media (min-width: 768px) {
+      padding-left: 8px;
+      padding-right: 8px;
+    }
+    @media (min-width: 1200px) {
+      padding-left: 4px;
+      padding-right: 0;
+    }
   }
 `;
 
